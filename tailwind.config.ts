e53491990bc74172e83ce4e8ca97fa117f71fd4b
Patrik Mintěl <ja@patrick115.eu>
2024-12-19 23:37:43 +0100

    Some basic things added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
@@ -6,8 +5,17 @@ export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
 
     theme: {
-        extend: {}
+        extend: {
+
+            colors: {
+                'text': 'rgb(242, 223, 222)',
+                'background': 'rgb(14, 2, 1)',
+                'primary': 'rgb(230, 159, 152)',
+                'secondary': 'rgb(130, 32, 23)',
+                'accent': 'rgb(247, 40, 8)',
+            },
+        }
     },
 
-    plugins: [typography, forms]
+    plugins: [typography]
 } satisfies Config;
