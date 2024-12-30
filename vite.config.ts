5ad842aa1dd8ded3045675547b2e1b213a2a0523
Patrik Mintěl <ja@patrick115.eu>
2024-12-30 23:49:56 +0100

    Added SveltePWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()]
+    plugins: [
+        sveltekit(),
+        SvelteKitPWA({
+            manifest: {
+                name: 'Family APP',
+                short_name: 'Family APP',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#0e0201',
+                lang: 'cs',
+                scope: '/'
+            }
+        })
+    ]
 });
