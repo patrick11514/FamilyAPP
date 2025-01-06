a583f7f2b1ce85b9c5c5c8c1c06d5f9053569f5b
patrick11514 <ja@patrick115.eu>
2025-01-06 03:48:35 +0100

    Added note

--- src/components/form/Entry.svelte
+++ src/components/form/Entry.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
     import type { Snippet } from 'svelte';
 
-    const { children, id = '', label, error = undefined }: { children: Snippet; id?: string; label: string; error?: string } = $props();
+    const {
+        children,
+        id = '',
+        label,
+        error = undefined,
+        auto = false,
+        note = ''
+    }: {
+        children: Snippet;
+        id?: string;
+        label: string;
+        error?: string;
+        auto?: boolean;
+        note?: string;
+    } = $props();
 </script>
 
-<div class="flex flex-col">
-    <label class="mb-2 text-xl font-bold lg:text-2xl" for={id}>{label}</label>
+<div class:w-full={!auto} class="flex flex-col">
+    <label class="mb-2 text-xl font-bold lg:text-2xl" for={id}>{label} <span class="text-base text-gray-500 lg:text-lg">{note}</span></label>
     {@render children()}
     <span class:invisible={!error} class="text-sm font-bold text-red-500">{error ?? '...'}</span>
 </div>
