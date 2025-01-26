1f055ea673575505c8c486f29b4eb7a4489a4963
Patrik Mintěl <ja@patrick115.eu>
2025-01-26 01:16:56 +0100

    Updated theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,17 @@ export default {
     theme: {
         extend: {
             colors: {
-                text: 'rgb(242, 223, 222)',
+                text: 'oklch(92.37% 0.011 17.35)',
+                background: 'oklch(0.00% 0.000 0.00)',
+                primary: 'oklch(90.37% 0.000 89.88)',
+                secondary: 'oklch(25.62% 0.000 89.88)',
+                accent: 'oklch(39.36% 0.162 29.23)'
+
+                /*text: 'rgb(242, 223, 222)',
                 background: 'rgb(14, 2, 1)',
                 primary: 'rgb(230, 159, 152)',
                 secondary: 'rgb(130, 32, 23)',
-                accent: 'rgb(247, 40, 8)'
+                accent: 'rgb(247, 40, 8)'*/
             },
             fontFamily: {
                 poppins: ['Poppins', 'sans-serif'],
