1bfe5ab7e137a2f7e3189c93378315e46241ecbf
Patrik Mintěl <ja@patrick115.eu>
2025-01-29 23:14:44 +0100

    Added recent events

--- src/routes/app/+layout.server.ts
+++ src/routes/app/+layout.server.ts
@@ -8,7 +8,30 @@ export const load = (async ({ cookies }) => {
         redirect(302, '/');
     }
 
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+
+    const afterWeek = new Date(startOfDay);
+    afterWeek.setDate(afterWeek.getDate() + 7);
+
     return {
+        recentEvents: conn
+            .selectFrom('calendar')
+            .selectAll()
+            .where((eb) => eb.or([eb.and([eb('to', '>=', startOfDay), eb('from', '<=', startOfDay)]), eb.and([eb('from', '>=', startOfDay), eb('from', '<', afterWeek)])]))
+            .limit(5)
+            .orderBy('from', 'desc')
+            .execute()
+            .then((events) =>
+                events.map((event) => {
+                    return {
+                        ...event,
+                        full_day: {
+                            data: [event.full_day.at(0)] as [0 | 1]
+                        }
+                    };
+                })
+            ),
         users: await conn.selectFrom('user').select(['id', 'username', 'firstname', 'lastname']).execute()
     };
 }) satisfies LayoutServerLoad;
--- src/routes/app/calendar/+page.svelte
+++ src/routes/app/calendar/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
     import { Table, Td, Th, Tr } from '$/components/table';
     import { API } from '$/lib/api';
-    import { Calendar, formatUser, SwalAlert, toLocalDateString, toTime } from '$/lib/functions';
+    import { Calendar, formatUser, SwalAlert, toLocalDateString } from '$/lib/functions';
     import { browser } from '$app/environment';
     import { onMount, untrack } from 'svelte';
     import type { PageData } from './$types';
-    import type { NormalizeId } from '$/types/types';
-    import type { Calendar as CalendarDB } from '$/types/database';
+    import { eventIsInDay, getEventName, getEventRange, type Event } from '$/lib/calendarUtils';
     import Title from '$/components/headers/Title.svelte';
     import Icon from '$/components/Icon.svelte';
     import { SvelteDate } from 'svelte/reactivity';
@@ -54,10 +53,6 @@
             window.removeEventListener('keydown', handleKeys);
         };
     });
-
-    type Event = Omit<NormalizeId<CalendarDB>, 'full_day'> & {
-        full_day: { data: [0 | 1] };
-    };
 
     let events = $state<Event[]>();
 
@@ -215,47 +210,6 @@
         return events;
     };
 
-    const getEventRange = (event: Event) => {
-        if (event.full_day.data[0] == 1) {
-            return 'Celodenní';
-        }
-
-        const startOfDay = new Date(selectedDay);
-        startOfDay.setHours(0, 0, 0, 0);
-        const endOfDay = new Date(selectedDay);
-        endOfDay.setHours(23, 59, 59, 999);
-
-        if (event.from.getTime() < startOfDay.getTime() && event.to.getTime() > endOfDay.getTime()) {
-            return 'Celodenní';
-        }
-
-        const start = event.from.getTime() < startOfDay.getTime() ? '00:00' : toTime(event.from);
-        const end = event.to.getTime() > endOfDay.getTime() ? '23:59' : toTime(event.to);
-
-        return `${start} - ${end}`;
-    };
-
-    const eventIsInDay = (date: Date, event: Event) => {
-        //so event is included in this day:
-        //1. event starts on this day
-        //2. event ends on this day
-        //3. event starts some days before and ends some days after
-
-        const startOfDay = new Date(date);
-        startOfDay.setHours(0, 0, 0, 0);
-        const endOfDay = new Date(date);
-        endOfDay.setHours(23, 59, 59, 999);
-
-        if (event.from.getTime() < startOfDay.getTime() && event.to.getTime() > endOfDay.getTime()) {
-            return true;
-        }
-
-        return (
-            (event.from.getDate() === date.getDate() && event.from.getMonth() === date.getMonth()) ||
-            (event.to.getDate() === date.getDate() && event.to.getMonth() === date.getMonth())
-        );
-    };
-
     const dateToGoogleCalendar = (date: Date) => {
         return date.toISOString().replace(/-|:|\.\d+/g, '');
     };
@@ -279,38 +233,6 @@
         }
 
         return `${BASE_URL}?${params.toString()}`;
-    };
-
-    const getEventName = (event: Event) => {
-        //if event is multi-day, we want to add to the end (1 day of 2) etc..
-        if (event.from.getDate() === event.to.getDate() && event.from.getMonth() === event.to.getMonth() && event.from.getFullYear() === event.to.getFullYear()) {
-            return event.name;
-        }
-
-        //event can go through multiple months
-        const begin = new Date(event.from);
-
-        let totalDays = 0;
-        let counting = true;
-        let currentDays = 1;
-
-        while (true) {
-            if (begin.getDate() === selectedDay.getDate() && begin.getMonth() === selectedDay.getMonth() && begin.getFullYear() === selectedDay.getFullYear()) {
-                counting = false;
-            }
-
-            if (begin.getDate() === event.to.getDate() && begin.getMonth() === event.to.getMonth() && begin.getFullYear() === event.to.getFullYear()) {
-                totalDays++;
-                break;
-            }
-
-            totalDays++;
-            if (counting) currentDays++;
-
-            begin.setDate(begin.getDate() + 1);
-        }
-
-        return `${event.name} (den ${currentDays} z ${totalDays})`;
     };
 
     let addingEvent = $state(false);
@@ -526,11 +448,11 @@
                     {#each sortEvents(eventsToday) as event (selectedDay.toString() + event.id)}
                         <div class="flex flex-col gap-1">
                             <div class="flex justify-between">
-                                <p class="font-bold">{getEventName(event)}</p>
+                                <p class="font-bold">{getEventName(event, selectedDay)}</p>
                                 <p>
                                     <Icon name="bi-clock-fill" />
 
-                                    {getEventRange(event)}
+                                    {getEventRange(event, selectedDay)}
                                 </p>
                             </div>
                             <p>{event.description}</p>
