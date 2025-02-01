d100fd117452122b3ccac53d86e88fc007511333
Patrik Mintěl <ja@patrick115.eu>
2025-02-01 18:04:49 +0100

    Format code

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,12 +8,12 @@ export type UserData = Omit<Selectable<User>, 'password'> & {
 
 export type UserState =
     | {
-        logged: false;
-    }
+          logged: false;
+      }
     | {
-        logged: true;
-        data: UserData;
-    };
+          logged: true;
+          data: UserData;
+      };
 
 export type Response = {
     status: true;
