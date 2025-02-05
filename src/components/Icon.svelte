ca285fc7b189737e20949edfcee38bf73ad9feaf
Patrik Mintěl <ja@patrick115.eu>
2025-02-05 01:19:30 +0100

    Make every page reloadable, by invalidateAll

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -3,9 +3,9 @@
     import type { BootstrapIcon } from '$/types/bootstrap_icons';
     import type { ClassValue, SvelteHTMLElements } from 'svelte/elements';
 
-    const { children, class: cls = '', name, ...rest }: SvelteHTMLElements['i'] & { class?: ClassValue; name: BootstrapIcon } = $props();
+    let { children, class: cls = '', name, self = $bindable(), ...rest }: SvelteHTMLElements['i'] & { class?: ClassValue; name: BootstrapIcon; self?: HTMLElement } = $props();
 </script>
 
-<i {...rest} role="button" tabindex="-1" class={['bi cursor-pointer', name, resolveSvelteClass(cls)]}>
+<i bind:this={self} {...rest} role="button" tabindex="-1" class={['bi cursor-pointer', name, resolveSvelteClass(cls)]}>
     {@render children?.()}
 </i>
