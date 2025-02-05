3be8addb11cad11125a5203e891752b49ad4fc62
Patrik Mintěl <ja@patrick115.eu>
2025-02-05 14:26:42 +0100

    Use funtion instead of duplicate code

--- src/routes/app/presents/[page=present]/+page.server.ts
+++ src/routes/app/presents/[page=present]/+page.server.ts
@@ -1,17 +1,21 @@
-import { conn, jwt } from '$/lib/server/variables';
-import type { UserData } from '$/types/types';
+import { getCookieData } from '$/lib/server/functions';
+import { conn } from '$/lib/server/variables';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ params, cookies, parent }) => {
     const parentData = await parent();
 
-    const cookie = cookies.get('session')!;
-    const userData = jwt.getCookie<UserData>(cookie)!;
+    const userData = getCookieData(cookies);
+
+    if (!userData.logged) {
+        redirect(302, '/app/presents/mine');
+    }
 
     const data = await conn
         .selectFrom('present')
         .selectAll()
-        .where('user_id', params.page === 'mine' ? '=' : '!=', userData.id)
+        .where('user_id', params.page === 'mine' ? '=' : '!=', userData.data.id)
         .orderBy('state', 'asc')
         .orderBy('id', 'desc')
         .execute();
