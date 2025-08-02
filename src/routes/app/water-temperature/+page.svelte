af82820e711c63c94b0cdb20db4ad5500f174260
Patrik Mintěl <ja@patrick115.eu>
2025-08-03 00:13:10 +0200

    Finished the graphs + loading data

---
+++ src/routes/app/water-temperature/+page.svelte
@@ -0,0 +1,243 @@
+<script lang="ts">
+    import MonthSelector from '$/components/form/MonthSelector.svelte';
+    import Icon from '$/components/Icon.svelte';
+    import { API } from '$/lib/api';
+    import { Calendar, SwalAlert } from '$/lib/functions';
+    import Chart from 'chart.js/auto';
+    import 'chartjs-adapter-date-fns';
+    import { onMount } from 'svelte';
+    import { SvelteDate } from 'svelte/reactivity';
+
+    let canvas: HTMLCanvasElement;
+    const calendar = new Calendar();
+
+    const today = new Date();
+    let year = $state(today.getFullYear());
+    let month = $state(today.getMonth());
+    let day = $state(today.getDate());
+    let maxDays = $state(calendar.getLastDayOfMonth(today).getDate());
+
+    let lastUpdate = new SvelteDate();
+    let lastUpper = $state(0);
+    let lastLower = $state(0);
+
+    $effect(() => {
+        maxDays = calendar.getLastDayOfMonth(new Date(year, month)).getDate();
+        if (day > maxDays) {
+            day = maxDays;
+        }
+    });
+
+    const nextDay = () => {
+        const date = new Date(year, month, day + 1);
+        if (date > today) return;
+        year = date.getFullYear();
+        month = date.getMonth();
+        day = date.getDate();
+    };
+
+    const previousDay = () => {
+        const date = new Date(year, month, day - 1);
+        year = date.getFullYear();
+        month = date.getMonth();
+        day = date.getDate();
+    };
+
+    const handleKeys = (ev: KeyboardEvent) => {
+        if (ev.key === 'ArrowLeft') {
+            previousDay();
+        } else if (ev.key === 'ArrowRight') {
+            nextDay();
+        }
+    };
+
+    let chart: Chart<'line', { x: Date; y: number }[]>;
+
+    const loadData = async (year: number, month: number, day: number) => {
+        if (!chart) return;
+        const data = await API.energyface({ year, month, day });
+        if (!data.status) {
+            SwalAlert({
+                icon: 'error',
+                title: 'Nepodařilo se načíst data'
+            });
+            return;
+        }
+        const { lower, upper } = data.data;
+        chart.data.datasets[0].data = lower;
+        chart.data.datasets[1].data = upper;
+
+        chart.update();
+
+        const _lastLower = lower[lower.length - 1];
+        const _lastUpper = upper[upper.length - 1];
+        const lastDate = new Date(
+            _lastLower.x > _lastUpper.x ? _lastLower.x : _lastUpper.x
+        );
+        lastUpdate.setHours(lastDate.getHours(), lastDate.getMinutes(), 0, 0);
+
+        lastLower = _lastLower.y;
+        lastUpper = _lastUpper.y;
+    };
+
+    onMount(() => {
+        window.addEventListener('keydown', handleKeys);
+
+        const baseConfig = {
+            lineTension: 0.3,
+            pointRadius: 0,
+            fill: false
+        };
+
+        chart = new Chart(canvas, {
+            type: 'line',
+            data: {
+                datasets: [
+                    {
+                        label: 'Voda dole',
+                        data: [],
+                        borderColor: 'oklch(62.3% 0.214 259.815)',
+                        backgroundColor: 'oklch(62.3% 0.214 259.815)',
+                        ...baseConfig
+                    },
+                    {
+                        label: 'Voda nahoře',
+                        data: [],
+                        borderColor: 'oklch(63.7% 0.237 25.331)',
+                        backgroundColor: 'oklch(63.7% 0.237 25.331)',
+                        ...baseConfig
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'minute',
+                            displayFormats: {
+                                minute: 'HH:mm',
+                                hour: 'HH:mm'
+                            },
+                            tooltipFormat: 'HH:mm'
+                        },
+                        ticks: {
+                            color: 'white',
+                            font: {
+                                size: 14,
+                                weight: 'bold'
+                            }
+                        }
+                    },
+                    y: {
+                        ticks: {
+                            color: 'white',
+                            font: {
+                                size: 14
+                            }
+                        }
+                    }
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+                interaction: {
+                    mode: 'index',
+                    intersect: false
+                },
+                animation: {
+                    duration: 0
+                },
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: (ctx) =>
+                                `${ctx.dataset.label}: ${ctx.formattedValue} °C`
+                        },
+                        titleFont: {
+                            size: 16
+                        },
+                        bodyFont: {
+                            size: 14
+                        },
+                        footerFont: {
+                            size: 12
+                        },
+                        backgroundColor: '#222', // optional: darker tooltip
+                        titleColor: '#fff',
+                        bodyColor: '#fff',
+                        footerColor: '#ccc'
+                    },
+                    legend: {
+                        labels: {
+                            color: 'white',
+                            font: {
+                                size: 14
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        loadData(year, month, day);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeys);
+
+            chart.destroy();
+        };
+    });
+
+    $effect(() => {
+        const date = new Date(year, month, day);
+        if (date > today) {
+            year = today.getFullYear();
+            month = today.getMonth();
+            day = today.getDate();
+            return;
+        }
+        loadData(year, month, day);
+    });
+
+    const resetDay = () => {
+        year = today.getFullYear();
+        month = today.getMonth();
+        day = today.getDate();
+    };
+</script>
+
+<section class="flex flex-1 flex-col">
+    <div class="flex w-full gap-2 text-2xl">
+        <Icon name="bi-bell" class="mr-auto" />
+        <Icon onclick={previousDay} name="bi-chevron-left" />
+        <select bind:value={year}>
+            <!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
+            {#each Array.from({ length: 10 }) as _, i (i)}
+                {@const y = year - 5 + i}
+                <option value={y}>{y}</option>
+            {/each}
+        </select>
+        <MonthSelector bind:value={month} />
+        <select bind:value={day}>
+            <!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
+            {#each Array.from({ length: maxDays }) as _, i (i)}
+                <option value={i + 1}>{i + 1}</option>
+            {/each}
+        </select>
+        <Icon onclick={nextDay} name="bi-chevron-right" />
+        <Icon name="bi-arrow-counterclockwise" onclick={resetDay} class="ml-auto" />
+    </div>
+    <div class="mx-auto flex flex-col text-2xl font-bold">
+        <h2>
+            Poslední aktualizace: {lastUpdate.toLocaleString('cs-CZ', {
+                hour: '2-digit',
+                minute: '2-digit'
+            })}
+        </h2>
+        <h2 class="text-red-500">Voda nahoře: {lastUpper} °C</h2>
+        <h2 class="text-blue-500">Voda dole: {lastLower} °C</h2>
+    </div>
+    <div class="flex-1">
+        <canvas bind:this={canvas}></canvas>
+    </div>
+</section>
